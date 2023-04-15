a3661ecb7a55ae1fcd052db14fbe145b598b8af1
SwethaReddySalkuti <salkutiswethareddy@gmail.com>
2023-04-15 17:55:10 +0530

    Read and Write Files

---
+++ file.js
@@ -0,0 +1,83 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req,res) => {
+    const url = req.url;
+    const method = req.method;
+    if(url==="/")
+    {
+        res.setHeader("Content-Type","text/html");
+        var x;
+       
+       
+        fs.readFile('message.txt',{ encoding:"utf-8" },(err,data) =>{
+           
+            x=data;
+            if(err)
+            {
+                console.log(err);
+            }
+        })
+        
+
+        res.write(
+            `
+            <!doctype html>
+            <html>
+            <head>
+            <title>File System Practise</title>
+            </head>
+            
+            <body>
+            ${x}
+            </body>
+            <body>
+            <form action="/message" method="post">
+            <div>
+            <label>Name : </label>
+            <input type="text" name="name"/>
+            <input type="submit" value="Submit"/>
+            
+            </div>
+            <div>
+            
+            </div>
+            </form>
+            </body>
+            </html>
+            `
+        )
+        
+        return res.end();
+    }    
+    else if(url==="/message" && method=== 'POST')
+    {
+        const body = [];
+        req.on('data',(chunk) => {
+            body.push(chunk);
+        });
+
+        return req.on('end',() => {
+            const requestBody = Buffer.concat(body).toString();
+            const message = requestBody.split('=')[1];
+            fs.writeFile('message.txt',message,(err) => {
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+
+            });
+
+        });
+
+        
+    }
+    else
+    {
+
+    }
+});
+
+
+
+
+server.listen(3500);
+//module.exports = requestHandler;
